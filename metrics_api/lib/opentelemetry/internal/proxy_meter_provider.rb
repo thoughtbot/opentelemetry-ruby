159341de880f250e8f8842cdb19e7cebe99e565b
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-06-30 17:39:41 -0300

    Update ProxyMeterProvider to match API

--- metrics_api/lib/opentelemetry/internal/proxy_meter_provider.rb
+++ metrics_api/lib/opentelemetry/internal/proxy_meter_provider.rb
@@ -13,45 +13,60 @@ module OpenTelemetry
     # It delegates to a "real" MeterProvider after the global meter provider is registered.
     # It returns {ProxyMeter} instances until the delegate is installed.
     class ProxyMeterProvider < Metrics::MeterProvider
-      Key = Struct.new(:name, :version)
-      private_constant(:Key)
+      def initialize(resource: nil)
+        super
 
-      # Returns a new {ProxyMeterProvider} instance.
-      #
-      # @return [ProxyMeterProvider]
-      def initialize
-        @mutex = Mutex.new
-        @registry = {}
         @delegate = nil
       end
 
       # Set the delegate Meter provider. If this is called more than once, a warning will
       # be logged and superfluous calls will be ignored.
       #
-      # @param [MeterProvider] provider The Meter provider to delegate to
-      def delegate=(provider)
-        unless @delegate.nil?
-          OpenTelemetry.logger.warn 'Attempt to reset delegate in ProxyMeterProvider ignored.'
-          return
-        end
-
+      # @param [MeterProvider] meter_provider The Meter provider to delegate to
+      def delegate=(meter_provider)
         @mutex.synchronize do
-          @delegate = provider
-          @registry.each { |key, meter| meter.delegate = provider.meter(key.name, version: key.version) }
+          if @delegate.nil?
+            @delegate = meter_provider
+
+            @meter_registry.each do |key, proxy_meter|
+              proxy_meter.delegate = meter_provider.meter(
+                proxy_meter.name,
+                version: proxy_meter.version,
+                schema_url: proxy_meter.schema_url,
+                attributes: proxy_meter.attributes,
+              )
+            end
+          else
+            OpenTelemetry.logger.warn 'Attempt to reset delegate in ProxyMeterProvider ignored.'
+          end
         end
       end
 
-      # Returns a {Meter} instance.
-      #
-      # @param [optional String] name Instrumentation package name
-      # @param [optional String] version Instrumentation package version
-      #
-      # @return [Meter]
-      def meter(name = nil, version: nil)
+      # @api private
+      def meter(name, version: nil, schema_url: nil, attributes: nil)
         @mutex.synchronize do
-          return @delegate.meter(name, version: version) unless @delegate.nil?
+          if @delegate.nil?
+            proxy_meter = ProxyMeter.new(
+              name,
+              version: version,
+              schema_url: schema_url,
+              attributes: attributes
+            )
+            key = Key.new(
+              proxy_meter.name,
+              proxy_meter.version,
+              proxy_meter.schema_url
+            )
 
-          @registry[Key.new(name, version)] ||= ProxyMeter.new
+            @meter_registry[key] ||= proxy_meter
+          else
+            @delegate.meter(
+              name,
+              version: version,
+              schema_url: schema_url,
+              attributes: attributes
+            )
+          end
         end
       end
     end
