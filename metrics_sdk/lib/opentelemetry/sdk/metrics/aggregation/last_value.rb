a76cb3ee0049bfdd9fea2720e2935dad10a329c2
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-08-13 17:35:31 -0300

    Add empty LastValue aggregation class

---
+++ metrics_sdk/lib/opentelemetry/sdk/metrics/aggregation/last_value.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Copyright The OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+module OpenTelemetry
+  module SDK
+    module Metrics
+      module Aggregation
+        class LastValue
+          # TODO: implement aggregation code
+          def initialize
+          end
+        end
+      end
+    end
+  end
+end
